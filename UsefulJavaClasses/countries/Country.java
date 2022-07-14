1279dec26837763cbbe030d5603d85ce07e7ec29
PETER ŽIVČÁK <peto.zivcak@gmail.com>
2022-07-14 15:28:52 +0200

    SchoolWork

---
+++ UsefulJavaClasses/countries/Country.java
@@ -0,0 +1,20 @@
+package countries;
+
+public class Country {
+    public String coutry;
+    public String shortcut;
+
+    public Country(String coutry, String shortcut) {
+        this.coutry = coutry;
+        this.shortcut = shortcut;
+
+
+
+}
+
+
+    }
+
+
+
+
---
+++ UsefulJavaClasses/countries/myTest.java
@@ -0,0 +1,10 @@
+package countries;
+
+public class myTest {
+}
+
+ Algeria	1548[1]	14[1]	Gendarme - 1055[1]
+ Angola	113[2]	112/116[2]	115[2]
+ Ascension Island	999[3]
+ Benin	117[4]	112[4]	118[4]
+ Burundi	117[5]	112[5]	118[5]
